d150ba8c3950a2c9a3ec935e2bdfade385959ac7
Unfit-Donkey <bjc2dogs@gmail.com>
2021-09-10 17:40:02 -0700

    initial files

---
+++ blackjack.cpp
@@ -0,0 +1,14 @@
+//Benjamin Cates, Damaris Sanchez, Suhkman Kaur, and Professor David Jones
+//2021
+//Blackjack probability calculator
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cout << "Blackjack simulator $0.99";
+
+
+
+    return 0;
+}
---
+++ simulator.cpp
@@ -0,0 +1,10 @@
+//Benjamin Cates, Damaris Sanchez, Suhkman Kaur and Profesor David Jones
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+
+
+}
